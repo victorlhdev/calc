0b555c99299c090a3468519b78ec9a4464f72784
victorlhdev <victorlhdev@gmail.com>
2022-02-08 00:21:33 -0300

    v1.1

--- js/principal.js
+++ js/principal.js
@@ -18,6 +18,34 @@ for (var i = 0; i < usuarios.length; i++) {
     tdSalarioLiquido.textContent = salarioLiquido.toFixed(2)
 };
 
+var botaoAdicionar = document.querySelector("#adicionar-usuario");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nomeUsuario = form.nomeForm.value;
+    var salarioBrutoUsuario = form.salarioBrutoForm.value;
+
+    var usuarioTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var salarioBrutoTd = document.createElement("td");
+    var descontoTd = document.createElement("td");
+    var salarioLiquidoTd = document.createElement("td");
+
+    nomeTd.textContent = nomeUsuario;
+    salarioBrutoTd.textContent = salarioBrutoUsuario;   
+
+    usuarioTr.appendChild(nomeTd);
+    usuarioTr.appendChild(salarioBrutoTd);
+    usuarioTr.appendChild(descontoTd);
+    usuarioTr.appendChild(salarioLiquidoTd);
+
+    var tabela = document.querySelector("#tabela-usuarios");
+    tabela.appendChild(usuarioTr);
+});
+
 
 
 
