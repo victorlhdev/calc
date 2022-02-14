1320320275f60387bbf65be652c6092b290915fc
victorlhdev <victorlhdev@gmail.com>
2022-02-14 18:17:09 -0300

    v1.7

--- js/remover-usuario.js
+++ js/remover-usuario.js
@@ -3,6 +3,9 @@ var tabela = document.querySelector("table");
 tabela.addEventListener("click", function (event) {
     var target = event.target;
     if (target.classList.contains('bi-x-square-fill')) {
-        target.closest('tr').remove();
+        target.closest('tr').classList.add("fadeOut");
+        setTimeout(function () {
+            target.closest('tr').remove();
+        }, 500);
     }
 });
